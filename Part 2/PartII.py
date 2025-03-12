ce07402bf1dd2bca0f158a4df2d59d8497167762
Yisi Liu <liu1857@mcmaster.ca>
2025-03-12 00:22:09 +0000

    Part 2 structure set up

---
+++ Part 2/PartII.py
@@ -0,0 +1,29 @@
+import matplotlib
+import numpy
+import random
+
+#utility functions
+#draw_plot()
+#create_random_graph()
+
+#2.1 Dijkstra's
+def dijkstra(graph, source, k):
+
+    #implementation
+
+    return
+
+
+#2.2 Bellman Ford's
+def bellman_ford(graph, source, k):
+
+    #implementation
+
+    return
+
+#2.3 Experiment
+def experiment_A():
+
+    #implementation
+
+    return
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Compsci2XC3Final
+
+Yisi Liu (liu1857) - 400494848
+Jinny Kim (kim630) - 400516637
+Rebecca Yang (yangr127) - 400514943 	 
+
+COMPSCI 2XC3	 	 		
+March - April 2025

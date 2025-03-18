59e0de608c12841f28a93b2e0f11306feff9b079
Yisi Liu <liu1857@mcmaster.ca>
2025-03-18 19:58:03 -0400

    using directed weighted graphs

--- Part 2/PartII.py
+++ Part 2/PartII.py
@@ -6,6 +6,7 @@ import random
 #draw_plot()
 
 #non-negative weights connected graph generator
+#NOT FOR THE CORRECT GRAPH CLASS WILL FIX LATER
 def create_random_graph(nodes, edges):
     # if edges exceed maximum, infinite loop
     max = nodes*(nodes-1)//2 #formula for undirected graph max edge number
@@ -29,53 +30,36 @@ def create_random_graph(nodes, edges):
 
     return graph
 
-#graph class (undirected weighted)
-class Graph():
-    # you may use the above weighted graph class here and modify it if necessary.
-    # aslo feel free to borrow any of the suitable graph class implementations discussed in class. 
-    # weighted (undirected) graph implementation from lecture notes:
-    def __init__(self, nodes):
-        self.graph = {}
-        self.weight = {}
-        for i in range(nodes):
-            self.graph[i] = []
+#graph class (directed weighted)
+class Graph:
+
+    def __init__(self):
+        self.adj = {}
+        self.weights = {}
 
     def are_connected(self, node1, node2):
-        for node in self.adj[node1]:
-            if node == node2:
+        for neighbour in self.adj[node1]:
+            if neighbour == node2:
                 return True
         return False
 
-    def connected_nodes(self, node):
-        return self.graph[node]
+    def adjacent_nodes(self, node):
+        return self.adj[node]
 
-    def add_node(self,):
-        #add a new node number = length of existing node
-        self.graph[len(self.graph)] = []
+    def add_node(self, node):
+        self.adj[node] = []
 
     def add_edge(self, node1, node2, weight):
-        if node1 not in self.graph[node2]:
-            self.graph[node1].append(node2)
-            self.weight[(node1, node2)] = weight
+        if node2 not in self.adj[node1]:
+            self.adj[node1].append(node2)
+        self.weights[(node1, node2)] = weight
 
-            #since it is undirected
-            self.graph[node2].append(node1)
-            self.weight[(node2, node1)] = weight
+    def w(self, node1, node2):
+        if self.are_connected(node1, node2):
+            return self.weights[(node1, node2)]
 
-    def number_of_nodes(self,):
-        return len(self.graph)
-
-    def has_edge(self, src, dst):
-        return dst in self.graph[src] 
-
-    def get_weight(self,):
-        total = 0
-        for node1 in self.graph:
-            for node2 in self.graph[node1]:
-                total += self.weight[(node1, node2)]
-                
-        # because it is undirected
-        return total/2
+    def number_of_nodes(self):
+        return len(self.adj)
 
 
 #2.1 Dijkstra's

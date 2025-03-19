e5ef4992f72e059226bd6a2a8db29222008ced08
jinnykim049 <kim630@mcmaster.ca>
2025-03-19 15:04:12 -0400

    2.1

--- Part 2/PartII.py
+++ Part 2/PartII.py
@@ -3,6 +3,7 @@ import numpy
 import random
 import math
 
+#---------------------------------Helper functions------------------------------------------------------------------------------------------------------------------
 #utility functions
 #draw_plot()
 
@@ -62,13 +63,111 @@ class Graph:
     def number_of_nodes(self):
         return len(self.adj)
 
+class Node:
+    def __init__(self, value, key=float('inf')):
+        self.value = value   
+        self.key = key     
+
+class MinHeap:
+    def __init__(self, data):
+        self.items = data 
+        self.length = len(data) 
+        self.map = {} 
+        self.build_heap()  
+        self.map = {}
+        for i in range(self.length):
+            self.map[self.items[i].value] = i
+
+    def find_left_index(self, index):
+        return 2 * (index + 1) - 1
+
+    def find_right_index(self, index):
+        return 2 * (index + 1)
+
+    def find_parent_index(self, index):
+        return (index + 1) // 2 - 1
+
+    def heapify(self, index):
+        smallest_known_index = index
+        if self.find_left_index(index) < self.length and self.items[self.find_left_index(index)].key < self.items[index].key:
+            smallest_known_index = self.find_left_index(index)
+        if self.find_right_index(index) < self.length and self.items[self.find_right_index(index)].key < self.items[smallest_known_index].key:
+            smallest_known_index = self.find_right_index(index)
+        if smallest_known_index != index:
+            self.items[index], self.items[smallest_known_index] = self.items[smallest_known_index], self.items[index]
+            self.map[self.items[index].value] = index
+            self.map[self.items[smallest_known_index].value] = smallest_known_index
+            self.heapify(smallest_known_index) 
+
+    def build_heap(self):
+        for i in range(self.length // 2 - 1, -1, -1): 
+            self.heapify(i)
+
+    def insert(self, node):
+        if len(self.items) == self.length:
+            self.items.append(node) 
+        else:
+            self.items[self.length] = node  
+        self.map[node.value] = self.length  
+        self.length += 1 
+        self.swim_up(self.length - 1)  
+
+    def extract_min(self):
+        self.items[0], self.items[self.length - 1] = self.items[self.length - 1], self.items[0]
+        self.map[self.items[self.length - 1].value] = self.length - 1
+        self.map[self.items[0].value] = 0
+        min_node = self.items[self.length - 1]
+        self.length -= 1 
+        self.map.pop(min_node.value)  
+        self.heapify(0) 
+        return min_node
+
+    def decrease_key(self, value, new_key):
+        if new_key >= self.items[self.map[value]].key:
+            return
+        index = self.map[value]  
+        self.items[index].key = new_key 
+        self.swim_up(index)  
+
+    def swim_up(self, index):
+        while index > 0 and self.items[index].key < self.items[self.find_parent_index(index)].key: 
+            self.items[index], self.items[self.find_parent_index(index)] = self.items[self.find_parent_index(index)], self.items[index]
+            self.map[self.items[index].value] = index
+            self.map[self.items[self.find_parent_index(index)].value] = self.find_parent_index(index)
+            index = self.find_parent_index(index)  
+
+    def is_empty(self):
+        return self.length == 0
+
+#----------------------------------------------------------Implementations---------------------------------------------------------------------------------------------------------------
 
 #2.1 Dijkstra's
 def dijkstra(graph, source, k):
 
-    #implementation
+    #Initialization
+    dist = {node: float('inf') for node in graph.adj}
+    prev = {node: None for node in graph.adj}
+    relax_count = {node: 0 for node in graph.adj}  # Record relaxation count 
+    dist[source] = 0
+    pq = MinHeap([Node(node, dist[node]) for node in graph.adj])
 
-    return
+    while not pq.is_empty():
+        u = pq.extract_min().value
+
+        # limit relaxation up to k times 
+        if relax_count[u] >= k:
+            continue
+
+        for v in graph.adj[u]:
+            alt = dist[u] + graph.weights[(u, v)]
+            if alt < dist[v]:
+                dist[v] = alt
+                prev[v] = u
+                pq.decrease_key(v, alt)
+                relax_count[v] += 1  # Increase relaxation count
+
+    return dist, prev 
+
 
 
 #2.2 Bellman Ford's

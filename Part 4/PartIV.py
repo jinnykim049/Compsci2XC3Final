3999e10d9505fb028eacac7e25b122f62c30ade7
Yisi Liu <liu1857@mcmaster.ca>
2025-03-27 20:06:59 -0400

    prelim a star implementation

--- Part 4/PartIV.py
+++ Part 4/PartIV.py
@@ -1,1 +1,72 @@
+import math
+import heapq
+
 #A star implementation
+#copied and modified from Part II Dijkstra's (credit to jinny!)
+
+class Node:
+    def __init__(self, value, key=float('inf')):
+        self.value = value   
+        self.key = key 
+    
+    def __lt__(self, other):
+        return self.key < other.key
+
+def A_Star(graph, source, destination, heuristic): 
+    #assumed heuristic to be a dict, change to round parenthesis for function
+
+    #Initialization
+    dist = {node: float('inf') for node in graph.adj}
+    prev = {node: None for node in graph.adj}
+    dist[source] = 0
+    pq = [Node(source, heuristic[source])]
+
+    while pq:
+        u = heapq.heappop(pq).value
+        
+        if u == destination:
+            return prev, dist[u]
+
+        for v in graph.adj[u]:
+            alt = dist[u] + graph.weights[(u, v)]
+            if alt < dist[v]:
+                dist[v] = alt
+                prev[v] = u
+                heapq.heappush(pq,Node(v,alt + heuristic[v]))
+                
+
+    return math.inf 
+
+#graph class (directed weighted)
+class Graph:
+
+    def __init__(self):
+        self.adj = {}
+        self.weights = {}
+
+    def are_connected(self, node1, node2):
+        for neighbour in self.adj[node1]:
+            if neighbour == node2:
+                return True
+        return False
+
+    def adjacent_nodes(self, node):
+        return self.adj[node]
+
+    def add_node(self, node):
+        self.adj[node] = []
+
+    def add_edge(self, node1, node2, weight):
+        if node2 not in self.adj[node1]:
+            self.adj[node1].append(node2)
+        self.weights[(node1, node2)] = weight
+
+    def w(self, node1, node2):
+        if self.are_connected(node1, node2):
+            return self.weights[(node1, node2)]
+
+    def number_of_nodes(self):
+        return len(self.adj)
+    
+    def has_edge(self,src,dst):
+        return dst in self.adj[src]

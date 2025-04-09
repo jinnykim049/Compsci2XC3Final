d74e9c675e7b253ec79375a1ab08eaa1150fd343
Yisi Liu <liu1857@mcmaster.ca>
2025-04-09 17:47:28 -0400

    creating part 6

---
+++ Part 6/PartVI.py
@@ -0,0 +1,7 @@
+class ShortPathFinder():
+    
+    def calc_short_path()
+    
+'''+calc_short_path(source:int, dest: int): float
++set_graph(graph: Graph):
++set_algorithm(algorithm: SPAlgorithm):'''
